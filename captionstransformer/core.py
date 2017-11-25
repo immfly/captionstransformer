47357f6f492377613539f4123edc113f49f5da6b
Sjur Fredriksen <sjurtf@ifi.uio.no>
2017-11-25 01:05:54 +0100

    undo

--- captionstransformer/core.py
+++ captionstransformer/core.py
@@ -37,7 +37,7 @@ class Reader(object):
 
 
 class Writer(object):
-    DOCUMENT_TPL = u"%s\n"
+    DOCUMENT_TPL = u"%s"
     CAPTION_TPL = u"%(start)s , %(end)s , %(text)s"
 
     def __init__(self, fobject, captions=None):
